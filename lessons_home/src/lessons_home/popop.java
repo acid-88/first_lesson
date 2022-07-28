5a379b389ba7013fdd6e954f4a7003aed4e4ed6f
1 <1@192.168.0.101>
2022-07-28 22:32:56 +0300

    Добавил сканер, анализируем вводимое значение

--- lessons_home/src/lessons_home/popop.java
+++ lessons_home/src/lessons_home/popop.java
@@ -1,8 +1,12 @@
 package lessons_home;
 
+import java.util.Scanner;
+
 public class popop {
 	public static void main(String args[]) {
-		int x = 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Введи число");
+		int x = scanner.nextInt();
 		while (x < 5) {
 		System.out.println("nice try");
 		System.out.println("Good Job");
