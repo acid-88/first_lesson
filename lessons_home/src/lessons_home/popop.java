b8a4a3763386df452afcf57797be72f1cfc3a09a
1 <1@192.168.0.101>
2022-07-28 22:11:17 +0300

    Добавил while и new string "end game"

--- lessons_home/src/lessons_home/popop.java
+++ lessons_home/src/lessons_home/popop.java
@@ -2,7 +2,12 @@ package lessons_home;
 
 public class popop {
 	public static void main(String args[]) {
+		int x = 0;
+		while (x < 5) {
 		System.out.println("nice try");
 		System.out.println("Good Job");
+		x = x + 1;
 	}
+		System.out.println("Конец игры!");
+	}		
 }
